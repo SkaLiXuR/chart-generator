10b5b0251096dab02f824a78d5fde275126739d5
Skqlixur <jmjmbo5@gmail.com>
2022-11-03 00:06:59 +0800

    Add base project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# chart-generator
-A website that generates charts
+# [Chart Generator](https://skalixur.github.io/chart-generator)
+
+A website that generates charts, I don't really have a better description for it.
---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  let chartTitle = '';
+  let chartContent2D = []
+  let rowLeft;
+  let rowRight;
+
+  function addRow() {
+    chartContent2D.push([rowLeft, rowRight])
+    chartContent2D = chartContent2D
+    console.log(rowLeft, rowRight, chartContent2D);
+  }
+
+  
+</script>
+
+<main>
+  <form on:submit|preventDefault={addRow}>
+    <div class="chart-controls">
+
+      <label for="chart-title-input">Chart title:</label>
+
+      <input autocomplete="off" 
+      placeholder="Chart Title" 
+      name="chart-title-input"
+      type="text" bind:value={chartTitle}>
+
+      <br>
+      
+      <label for="row-left-input">Add a row:</label>
+
+      <input autocomplete="off" 
+      placeholder="Left side" 
+      name="row-left-input" 
+      type="text" bind:value={rowLeft}>
+
+      <input autocomplete="off" 
+      placeholder="Right side" 
+      type="text" bind:value={rowRight}>
+
+      <input type="submit" value="Add Row" on:submit={addRow}>
+    </div>
+  </form>
+    <div class="chart-container">
+      <div class="chart-title chart-item">{chartTitle}</div>
+      {#each chartContent2D as elem}
+      <div class="chart-item">{elem[0]}</div>
+      <div class="chart-item">{elem[1]}</div>
+      {/each}
+    </div>
+</main>
